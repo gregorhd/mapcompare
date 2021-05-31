d82cda68caaa6818ae8d4f61f51be276047cf3e1
Gregor <gregorherda@gmail.com>
2021-05-31 08:48:46 +0200

    main.py for PostGIS and plotting, dem_load.py for DEM ident

--- main.py
+++ main.py
@@ -1,56 +1,63 @@
 import numpy as np
 import geopandas as gpd
-import rasterio as rio
-from cartopy import crs as ccrs
-from cartopy.feature import ShapelyFeature
 import matplotlib.pyplot as plt
+from cartopy import crs as ccrs
 from sqlalchemy import create_engine
-import timeit
 import time
 import functools
-from dem_load import findDGM20s, showDEMs
+import demload
 from datetime import datetime
 from misc.pw import password
-
 
 def timer(func):
     """Print runtime of decorated function courtesy of RealPython's Primer on Python Decorators: https://realpython.com/primer-on-python-decorators/"""
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
-        start_time = time.perf_counter() #1
+        start_time = time.perf_counter()
         value = func(*args, **kwargs)
-        end_time = time.perf_counter() #2
-        run_time = end_time - start_time #3
+        end_time = time.perf_counter() 
+        run_time = end_time - start_time
         print(f"\nFinished {func.__name__!r} in {run_time:.4f} secs")
         return value
     return wrapper_timer
 
 # Connection with SQLAlchemy
 
-def alchemyConnect(password):
+@timer
+def alchemyConnect(db_name, password):
     """
     """
 
-    db_connection_url = "postgresql://postgres:" + password + "@localhost:5432/msc_test"
+    db_connection_url = "postgresql://postgres:" + password + "@localhost:5432/" + db_name
 
-    con = create_engine(db_connection_url)  
+    con = create_engine(db_connection_url)
 
-    sql1 = "SELECT DISTINCT g.gml_id, g.gebaeudefunktion, g.wkb_geometry as geom FROM public.ax_gebaeude as g JOIN public.ax_fliessgewaesser as f ON ST_DWithin(g.wkb_geometry, f.wkb_geometry, 500)";
+    # Find buildings within 500 meters of rivers
+    # 96msec on msc_test, 5.6sec on msc
+    # 101,697 rows
+    sql1 = "WITH buffer as (SELECT ST_Union(ST_Buffer(f.wkb_geometry, 300)) as geom FROM public.ax_fliessgewaesser as f) SELECT g.gebaeudefunktion, g.wkb_geometry as geom FROM public.ax_gebaeude as g JOIN buffer ON ST_Within(g.wkb_geometry, buffer.geom) OR ST_Intersects(g.wkb_geometry, buffer.geom);"
 
     buildings_in = gpd.GeoDataFrame.from_postgis(sql1, con, crs='epsg:25833')
 
-    sql2 = "SELECT gml_id, gebaeudefunktion, wkb_geometry as geom FROM public.ax_gebaeude WHERE gml_id NOT IN (SELECT DISTINCT g.gml_id FROM public.ax_gebaeude as g JOIN public.ax_fliessgewaesser as f ON ST_DWithin(g.wkb_geometry, f.wkb_geometry, 500));"
+    # Find all other buildings - is there a more efficient way?
+    # 220msec on msc_test, 1min29msc on msc
+    # 41,878 rows
+    sql2 = "WITH buffer as (SELECT ST_Union(ST_Buffer(f.wkb_geometry, 300)) as geom FROM public.ax_fliessgewaesser as f) SELECT g.gebaeudefunktion, g.wkb_geometry as geom FROM public.ax_gebaeude as g JOIN buffer ON ST_Disjoint(buffer.geom, g.wkb_geometry) WHERE g.gml_id NOT IN (WITH buffer as (SELECT ST_Union(ST_Buffer(f.wkb_geometry, 300)) as geom FROM public.ax_fliessgewaesser as f) SELECT g.gml_id FROM public.ax_gebaeude as g JOIN buffer ON ST_Within(g.wkb_geometry, buffer.geom) OR ST_Intersects(g.wkb_geometry, buffer.geom));" 
 
     buildings_out = gpd.GeoDataFrame.from_postgis(sql2, con, crs='epsg:25833')
 
-    sql3 = "SELECT gml_id, wkb_geometry as geom FROM public.ax_fliessgewaesser;"
+    # Query 3: Find all rivers
+    # 70-90msec on msc_test and msc
+    # 1,152 rows
+    sql3 = "SELECT wkb_geometry as geom FROM public.ax_fliessgewaesser;"
 
     rivers = gpd.GeoDataFrame.from_postgis(sql3, con, crs='epsg:25833')
 
     return buildings_in, buildings_out, rivers
 
+@timer
 def getBBox(*gdfs):
-    """Return a list object for cartopy set_extent (x0, x1, y0, y1).
+    """Return combined bbox of all GDFs in cartopy set_extent format (x0, x1, y0, y1).
     """
 
     list_of_bounds = []
@@ -68,7 +75,9 @@ def getBBox(*gdfs):
     return carto_extent
 
 if __name__ == "__main__":
-    buildings_in, buildings_out, rivers = alchemyConnect(password) # timeit: 214 ms ± 52.5 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
+    db_name = 'msc_test'
+    
+    buildings_in, buildings_out, rivers = alchemyConnect(db_name, password) # timeit: 214 ms ± 52.5 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
 
     crs = ccrs.epsg('25833')
 
@@ -78,28 +87,19 @@ if __name__ == "__main__":
 
     ax.set_extent(carto_extent, crs=crs)
 
-    # to clean up...
+    csvpath = r'c:\Users\grego\OneDrive\01_GIS\11_MSc\00_Project\01_data\02_DEM\DGM20\dgm25_akt.csv'
 
-    rast_list = findDGM20s(r'c:\Users\grego\OneDrive\01_GIS\11_MSc\00_Project\01_data\02_DEM\DGM20\dgm25_akt.csv', carto_extent)
+    data_dir = '01_data/02_DEM/DGM20/'
 
-    file_list = []
+    handles, ax = demload.showDEMs(csvpath, data_dir, carto_extent, ax, crs)
 
-    for tile in rast_list:
-        tile_num = tile[0]
-        filename = '33' + tile_num + '_dgm20.xyz'
-        file_list.append(filename)
+    ax.add_geometries(buildings_in.geometry, crs=crs, facecolor='red')
 
-    # cont...
+    ax.add_geometries(buildings_out.geometry, crs=crs, facecolor='white', edgecolor='black', linewidth=0.2)
 
-    handles, ax = showDEMs(file_list, ax, crs)
+    ax.add_geometries(rivers.geometry, crs=crs, facecolor='lightblue', edgecolor=None)
 
-    ax.add_geometries(buildings_in.geometry, crs=crs, facecolor='red', edgecolor='red')
-
-    ax.add_geometries(buildings_out.geometry, crs=crs, facecolor='white', edgecolor='grey', alpha=0.5)
-
-    ax.add_geometries(rivers.geometry, crs=crs, facecolor='lightblue', edgecolor='blue')
-
-    # plt.savefig('02_outputs/01_matplotlib/' + datetime.today().strftime('%Y-%m-%d') + '.jpg', format='jpg', dpi=600.0, orientation='landscape')
+    plt.savefig('02_outputs/01_matplotlib/' + datetime.today().strftime('%Y-%m-%d') + ' ' + db_name + '.svg', format='svg', orientation='landscape')
 
 
 
